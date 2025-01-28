08919f119c2d153ee27c13352c4aa929a9a07d0d
j-Karnika <jayanthkarnika@gmail.com>
2025-01-28 18:38:06 +0530

    ADD : Hexagon search

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import AtomicPageWrapper from "./searchpage.tsx";
 
+
 function App() {
   return (
     <>
       <div>
         {/* <h1>My coveo search page.</h1> */}
         <AtomicPageWrapper
-          accessToken={"xxec3577c4-49fb-4e3c-9b69-da83b08d8289"}
-          organizationId={"pwbmknrr4rtrycvugyuxwfn2fcy"}
-          pipeline={"GlobalPipeline_test"}
-          searchHub={"globalpipelinetest"}
+          accessToken={process.env.REACT_APP_ACCESS_TOKEN || ''}
+          organizationId={process.env.REACT_APP_ORGANIZATION_ID || ''}
+          pipeline={process.env.REACT_APP_PIPELINE || ''}
+          searchHub={process.env.REACT_APP_SEARCH_HUB || ''}
         />
       </div>
     </>
   );
 }
-
 export default App;
